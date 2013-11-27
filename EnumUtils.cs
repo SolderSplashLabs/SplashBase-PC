f6941707cdd162c57f32e8b3ac30627b1e52c9c8
carl <carl@soldersplash.co.uk>
2013-11-27 01:13:16 +0000

    missing files added

---
+++ EnumUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.ComponentModel;
+
+// Using Example from : http://blog.waynehartman.com/articles/84.aspx
+
+namespace SplashBaseControl
+{
+    class EnumUtils
+    {
+        public static string stringValueOf(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+
+        public static object enumValueOf(string value, Type enumType)
+        {
+            string[] names = Enum.GetNames(enumType);
+            foreach (string name in names)
+            {
+                if (stringValueOf((Enum)Enum.Parse(enumType, name)).Equals(value))
+                {
+                    return Enum.Parse(enumType, name);
+                }
+            }
+
+            throw new ArgumentException("The string is not a description or value of the specified enum.");
+        }
+    }
+}
